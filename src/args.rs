14178335a98abf3074a731007e903e7b3587dfe1
Niklas Treml <treml.niklas@gmail.com>
2023-07-11 23:07:46 +0200

    chore: add some metadata to cargo.toml

--- src/args.rs
+++ src/args.rs
@@ -1,10 +1,9 @@
 use clap::Parser;
 
-use crate::app::App;
-
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
+    // Set this to your remote's name, if you're using a different name than 'origin'
     #[arg(short, long, default_value_t = String::from("origin"))]
     pub remote: String,
 }
